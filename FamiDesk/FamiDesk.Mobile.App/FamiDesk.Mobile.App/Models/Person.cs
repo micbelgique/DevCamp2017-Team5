5380df21c0a8acb8aa10de44943f297a9911dc7d
Vincent Margan <VMA@Pulsar.be>
2017-07-28 23:55:01 +0200

    PersonAzureDataStore added

---
+++ FamiDesk/FamiDesk.Mobile.App/FamiDesk.Mobile.App/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿namespace FamiDesk.Mobile.App.Models
+{
+    public class Person : BaseDataObject
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _beaconId;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set => SetProperty(ref _firstName, value);
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set => SetProperty(ref _lastName, value);
+        }
+
+        public string BeaconId
+        {
+            get => _beaconId;
+            set => SetProperty(ref _beaconId, value);
+        }
+    }
+}
--- FamiDesk/FamiDesk.Mobile.App/FamiDesk.Mobile.App/Services/AzureDataStore.cs
+++ FamiDesk/FamiDesk.Mobile.App/FamiDesk.Mobile.App/Services/AzureDataStore.cs
@@ -13,17 +13,18 @@ using Plugin.Connectivity;
 
 namespace FamiDesk.Mobile.App.Services
 {
-    public class AzureDataStore : IDataStore<Item>
+
+    public abstract class AzureDataStore<T> : IDataStore<T> where T : BaseDataObject
     {
         public bool UseAuthentication => false;
         public MobileServiceAuthenticationProvider AuthProvider => MobileServiceAuthenticationProvider.Facebook;
 
         bool isInitialized;
-        IMobileServiceSyncTable<Item> itemsTable;
+        IMobileServiceSyncTable<T> itemsTable;
 
         public MobileServiceClient MobileService { get; set; }
 
-        public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
+        public async Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false)
         {
             await InitializeAsync();
             if (forceRefresh)
@@ -32,7 +33,7 @@ namespace FamiDesk.Mobile.App.Services
             return await itemsTable.ToEnumerableAsync();
         }
 
-        public async Task<Item> GetItemAsync(string id)
+        public async Task<T> GetItemAsync(string id)
         {
             await InitializeAsync();
             await PullLatestAsync();
@@ -44,7 +45,7 @@ namespace FamiDesk.Mobile.App.Services
             return items[0];
         }
 
-        public async Task<bool> AddItemAsync(Item item)
+        public async Task<bool> AddItemAsync(T item)
         {
             await InitializeAsync();
             await PullLatestAsync();
@@ -54,7 +55,7 @@ namespace FamiDesk.Mobile.App.Services
             return true;
         }
 
-        public async Task<bool> UpdateItemAsync(Item item)
+        public async Task<bool> UpdateItemAsync(T item)
         {
             await InitializeAsync();
             await itemsTable.UpdateAsync(item);
@@ -63,7 +64,7 @@ namespace FamiDesk.Mobile.App.Services
             return true;
         }
 
-        public async Task<bool> DeleteItemAsync(Item item)
+        public async Task<bool> DeleteItemAsync(T item)
         {
             await InitializeAsync();
             await PullLatestAsync();
@@ -98,9 +99,9 @@ namespace FamiDesk.Mobile.App.Services
             }
 
             var store = new MobileServiceSQLiteStore("app.db");
-            store.DefineTable<Item>();
+            store.DefineTable<T>();
             await MobileService.SyncContext.InitializeAsync(store, new MobileServiceSyncHandler());
-            itemsTable = MobileService.GetSyncTable<Item>();
+            itemsTable = MobileService.GetSyncTable<T>();
 
             isInitialized = true;
         }
@@ -114,7 +115,7 @@ namespace FamiDesk.Mobile.App.Services
             }
             try
             {
-                await itemsTable.PullAsync($"all{typeof(Item).Name}", itemsTable.CreateQuery());
+                await itemsTable.PullAsync($"all{typeof(T).Name}", itemsTable.CreateQuery());
             }
             catch (Exception ex)
             {
@@ -170,4 +171,163 @@ namespace FamiDesk.Mobile.App.Services
             return true;
         }
     }
+    #region Original
+    //public class AzureDataStore : IDataStore<Item>
+    //{
+    //    public bool UseAuthentication => false;
+    //    public MobileServiceAuthenticationProvider AuthProvider => MobileServiceAuthenticationProvider.Facebook;
+
+    //    bool isInitialized;
+    //    IMobileServiceSyncTable<Item> itemsTable;
+
+    //    public MobileServiceClient MobileService { get; set; }
+
+    //    public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
+    //    {
+    //        await InitializeAsync();
+    //        if (forceRefresh)
+    //            await PullLatestAsync();
+
+    //        return await itemsTable.ToEnumerableAsync();
+    //    }
+
+    //    public async Task<Item> GetItemAsync(string id)
+    //    {
+    //        await InitializeAsync();
+    //        await PullLatestAsync();
+    //        var items = await itemsTable.Where(s => s.Id == id).ToListAsync();
+
+    //        if (items == null || items.Count == 0)
+    //            return null;
+
+    //        return items[0];
+    //    }
+
+    //    public async Task<bool> AddItemAsync(Item item)
+    //    {
+    //        await InitializeAsync();
+    //        await PullLatestAsync();
+    //        await itemsTable.InsertAsync(item);
+    //        await SyncAsync();
+
+    //        return true;
+    //    }
+
+    //    public async Task<bool> UpdateItemAsync(Item item)
+    //    {
+    //        await InitializeAsync();
+    //        await itemsTable.UpdateAsync(item);
+    //        await SyncAsync();
+
+    //        return true;
+    //    }
+
+    //    public async Task<bool> DeleteItemAsync(Item item)
+    //    {
+    //        await InitializeAsync();
+    //        await PullLatestAsync();
+    //        await itemsTable.DeleteAsync(item);
+    //        await SyncAsync();
+
+    //        return true;
+    //    }
+
+    //    public async Task InitializeAsync()
+    //    {
+    //        if (isInitialized)
+    //            return;
+
+    //        AuthenticationHandler handler = null;
+
+    //        if (UseAuthentication)
+    //            handler = new AuthenticationHandler();
+
+    //        MobileService = new MobileServiceClient(App.AzureMobileAppUrl, handler)
+    //        {
+    //            SerializerSettings = new MobileServiceJsonSerializerSettings
+    //            {
+    //                CamelCasePropertyNames = true
+    //            }
+    //        };
+
+    //        if (UseAuthentication && !string.IsNullOrWhiteSpace(Settings.AuthToken) && !string.IsNullOrWhiteSpace(Settings.UserId))
+    //        {
+    //            MobileService.CurrentUser = new MobileServiceUser(Settings.UserId);
+    //            MobileService.CurrentUser.MobileServiceAuthenticationToken = Settings.AuthToken;
+    //        }
+
+    //        var store = new MobileServiceSQLiteStore("app.db");
+    //        store.DefineTable<Item>();
+    //        await MobileService.SyncContext.InitializeAsync(store, new MobileServiceSyncHandler());
+    //        itemsTable = MobileService.GetSyncTable<Item>();
+
+    //        isInitialized = true;
+    //    }
+
+    //    public async Task<bool> PullLatestAsync()
+    //    {
+    //        if (!CrossConnectivity.Current.IsConnected)
+    //        {
+    //            Debug.WriteLine("Unable to pull items, we are offline");
+    //            return false;
+    //        }
+    //        try
+    //        {
+    //            await itemsTable.PullAsync($"all{typeof(Item).Name}", itemsTable.CreateQuery());
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Debug.WriteLine("Unable to pull items, that is alright as we have offline capabilities: " + ex);
+    //            return false;
+    //        }
+    //        return true;
+    //    }
+
+
+    //    public async Task<bool> SyncAsync()
+    //    {
+    //        if (!CrossConnectivity.Current.IsConnected)
+    //        {
+    //            Debug.WriteLine("Unable to sync items, we are offline");
+    //            return false;
+    //        }
+    //        try
+    //        {
+    //            await MobileService.SyncContext.PushAsync();
+    //            if (!(await PullLatestAsync().ConfigureAwait(false)))
+    //                return false;
+    //        }
+    //        catch (MobileServicePushFailedException exc)
+    //        {
+    //            if (exc.PushResult == null)
+    //            {
+    //                Debug.WriteLine("Unable to sync, that is alright as we have offline capabilities: " + exc);
+    //                return false;
+    //            }
+    //            foreach (var error in exc.PushResult.Errors)
+    //            {
+    //                if (error.OperationKind == MobileServiceTableOperationKind.Update && error.Result != null)
+    //                {
+    //                    //Update failed, reverting to server's copy.
+    //                    await error.CancelAndUpdateItemAsync(error.Result);
+    //                }
+    //                else
+    //                {
+    //                    // Discard local change.
+    //                    await error.CancelAndDiscardItemAsync();
+    //                }
+
+    //                Debug.WriteLine(@"Error executing sync operation. Item: {0} ({1}). Operation discarded.", error.TableName, error.Item["id"]);
+    //            }
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Debug.WriteLine("Unable to sync items, that is alright as we have offline capabilities: " + ex);
+    //            return false;
+    //        }
+
+    //        return true;
+    //    }
+    //} 
+    #endregion
 }
