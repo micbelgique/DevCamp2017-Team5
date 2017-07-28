527b512031462e935540fc47ed969e5dc9f21c6a
Corentin Leruth <tinencor@gmail.com>
2017-07-28 21:34:47 +0200

    some renaming

--- FamiDesk/FamiDesk.Mobile.App/FamiDesk.Mobile.App.MobileAppService/Controllers/EventController.cs
+++ FamiDesk/FamiDesk.Mobile.App/FamiDesk.Mobile.App.MobileAppService/Controllers/EventController.cs
@@ -5,85 +5,54 @@ using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.OData;
 using Microsoft.Azure.Mobile.Server;
-
 using FamiDesk.Mobile.App.MobileAppService.DataObjects;
 using FamiDesk.Mobile.App.MobileAppService.Models;
 using System;
 
 namespace FamiDesk.Mobile.App.MobileAppService.Controllers
 {
-    // TODO: Uncomment [Authorize] attribute to require user be authenticated to access Item(s).
+    // TODO: Uncomment [Authorize] attribute to require user be authenticated to access Event(s).
     // [Authorize]
     public class EventController : TableController<Event>
     {
-		MasterDetailContext context;
-		protected override void Initialize(HttpControllerContext controllerContext)
+        MasterDetailContext context;
+
+        protected override void Initialize(HttpControllerContext controllerContext)
         {
             base.Initialize(controllerContext);
             context = new MasterDetailContext();
             DomainManager = new EntityDomainManager<Event>(context, Request);
         }
 
-        // GET tables/Item
-        public IQueryable<Event> GetAllItems()
+        // GET tables/Event
+        public IQueryable<Event> GetAllEvents()
         {
             return Query();
         }
 
-        // GET tables/Item/48D68C86-6EA6-4C25-AA33-223FC9A27959
-        public SingleResult<Event> GetItem(string id)
-        {
-            return Lookup(id);
-        }
+        // GET tables/Event/48D68C86-6EA6-4C25-AA33-223FC9A27959
+//        public SingleResult<Event> GetEvent(string id)
+//        {
+//            return Lookup(id);
+//        }
 
-        // PATCH tables/Item/48D68C86-6EA6-4C25-AA33-223FC9A27959
-        public Task<Event> PatchItem(string id, Delta<Event> patch)
+        // PATCH tables/Event/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task<Event> PatchEvent(string id, Delta<Event> patch)
         {
             return UpdateAsync(id, patch);
         }
 
-        // POST tables/Item
-        public async Task<IHttpActionResult> PostItem(Event item)
+        // POST tables/Event
+        public async Task<IHttpActionResult> PostEvent(Event item)
         {
-			Event current = await InsertAsync(item);
-            return CreatedAtRoute("Tables", new { id = current.Id }, current);
+            Event current = await InsertAsync(item);
+            return CreatedAtRoute("Tables", new {id = current.Id}, current);
         }
 
-        // DELETE tables/Item/48D68C86-6EA6-4C25-AA33-223FC9A27959
-        public Task DeleteItem(string id)
+        // DELETE tables/Event/48D68C86-6EA6-4C25-AA33-223FC9A27959
+        public Task DeleteEvent(string id)
         {
             return DeleteAsync(id);
         }
-
-		public async Task CheckIn(string personId, string comment)
-		{
-			var person = await context.Persons.FindAsync(personId);
-			if (person == null)
-				NotFound();
-
-			var item = new Event {
-				Id = Guid.NewGuid().ToString(),
-				Date = DateTime.UtcNow,
-				Type = EEventType.CheckIn,
-				Comment = comment,
-				Person = person
-			};
-		}
-
-		public async Task CheckOut(string personId, string comment)
-		{			
-			var person = await context.Persons.FindAsync(personId);
-			if (person == null)
-				NotFound();
-
-			var item = new Event
-			{
-				Id = Guid.NewGuid().ToString(),
-				Date = DateTime.UtcNow,
-				Type = EEventType.CheckIn,
-				Comment = comment,
-				Person = person
-			};
-		}
-	}
+    }
 }
