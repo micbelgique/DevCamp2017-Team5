7af254d71cd26d339cbf6c26fffdbaa634e75ff8
Fabian Allardos <fabian.allardos@gmail.com>
2017-07-28 17:48:09 +0200

    base entities

---
+++ FamiDesk/FamiDesk.Mobile.App/FamiDesk.Mobile.App.MobileAppService/DataObjects/EEventType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FamiDesk.Mobile.App.MobileAppService.DataObjects
+{
+	public enum EEventType : byte
+	{
+		CheckIn,
+		CheckOut
+	}
+}
