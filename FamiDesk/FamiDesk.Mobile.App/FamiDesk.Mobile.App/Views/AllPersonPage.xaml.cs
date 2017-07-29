82e9883def18c848cccb76df29373c78fd0c7b05
Vincent Margan <VMA@Pulsar.be>
2017-07-29 09:11:02 +0200

    all persons view and viewModel implementation

---
+++ FamiDesk/FamiDesk.Mobile.App/FamiDesk.Mobile.App/Views/AllPersonPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FamiDesk.Mobile.App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AllPersonPage : ContentPage
+    {
+        public AllPersonPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
