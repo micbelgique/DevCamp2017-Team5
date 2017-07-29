bd4941271d1600f428b3523b4d2349a96b65f697
Fabian Allardos <fabian.allardos@gmail.com>
2017-07-29 19:08:19 +0200

    ...

---
+++ FamiDesk/FamiDesk.Mobile.App/FamiDesk.Mobile.App/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FamiDesk.Mobile.App.Helpers;
+using FamiDesk.Mobile.App.Models;
+using FamiDesk.Mobile.App.Views;
+using Xamarin.Forms;
+
+namespace FamiDesk.Mobile.App.ViewModels
+{
+	public class DetailsViewModel : BaseViewModel
+	{		
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Address { get; set; }
+		public string Avatar { get; set; }
+
+		public DetailsViewModel()
+		{
+			Title = "Details";
+		}
+
+		public void Load(Person person)
+		{
+			FirstName = person.FirstName;
+			LastName = person.LastName;
+			Address = person.Address;
+			Avatar = person.Avatar;
+		}
+	}
+}
---
+++ FamiDesk/FamiDesk.Mobile.App/FamiDesk.Mobile.App/Views/DetailsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using FamiDesk.Mobile.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FamiDesk.Mobile.App.ViewModels;
+
+namespace FamiDesk.Mobile.App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailsPage : ContentPage
+    {
+		private readonly DetailsViewModel viewModel;
+		public DetailsPage(Person person)
+        {			
+			InitializeComponent();
+			viewModel = new DetailsViewModel();
+			viewModel.Load(person);
+			BindingContext = viewModel;
+		}
+	}
+}
